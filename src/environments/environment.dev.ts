1f4b8c0b52b60cdcc1706f4f96d6b7c91ac0f17c
rantoine <aruffino@nextoo.fr>
2018-07-06 13:42:51 +0200

    rectification uniquement , pour les prix

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   api_url: 'https://ecommerce-api-dev.nextoo.fr/',
+  api_login_url: 'https://ecommerce-api-prod.nextoo.fr/login',
   api_rest_upload_url: 'http://ecommerce-api-dev.nextoo.fr/upload',
   api_rest_download_url: 'http://ecommerce-api-dev.nextoo.fr/fichier/'
 };
