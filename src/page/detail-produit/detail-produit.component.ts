f055a81eb860da30b79b8560029adc6cc8a24d65
rantoine <aruffino@nextoo.fr>
2018-07-12 11:47:40 +0200

    Realisation US#356

--- src/page/detail-produit/detail-produit.component.ts
+++ src/page/detail-produit/detail-produit.component.ts
@@ -170,8 +170,11 @@ export class DetailProduitComponent implements OnInit {
   public async updateProduct() {
     if (this.photoEnAttenteSupression !== undefined) {
       for (const photo of this.photoEnAttenteSupression) {
-        console.log(this.photoEnAttenteSupression);
-        this.produitBusiness.removePhoto(photo);
+        await this.produitBusiness.removePhoto(photo);
+        if (photo.id === this.produitModifie.photoPrincipale.id){
+          this.produitModifie.photoPrincipale.id = 0;
+          this.produit.photoPrincipale.id =0;
+        }
       }
       this.photoEnAttenteSupression = [];
     }
@@ -298,14 +301,8 @@ export class DetailProduitComponent implements OnInit {
   }
 
   numberToCommaSeperate(event) {
-    // if (event.key === '.') {
-    //   this.produitModifie.prixHT =  this.ancienPrixHTModifier;
-    // } else {
-    //   this.ancienPrixHTModifier = this.produitModifie.prixHT;
-    // }
     const pattern = /^[0-9,]+$/;
-    let inputChar = String.fromCharCode(event.charCode);
-    console.log(!pattern.test(inputChar));
+    const inputChar = String.fromCharCode(event.charCode);
     if (!pattern.test(inputChar)) {
       // invalid character, prevent input
       event.preventDefault();
@@ -336,8 +333,8 @@ export class DetailProduitComponent implements OnInit {
   }
 
   /**
-   * Methode permettant d'initialisé la suppression d'une photo qui se fera lors de la sauvegarde
-   * @param {Photo} photo
+   * Methode permettant de transformer la photo en photo principale du produit
+   * @param {Photo} photo la photo qui va devenir principale
    */
   public favPhoto(photo: Photo): void {
     this.produitModifie.photoPrincipale.id = photo.id;
