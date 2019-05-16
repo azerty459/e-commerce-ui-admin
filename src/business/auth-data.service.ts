1f8b788c1d273b8e11bbb02bd4f6abf207ff4e7c
Arthur Brandao <abrandao@nextoo.fr>
2019-05-16 17:16:45 +0200

    Amélioration authentification et récupération info de l'utilisateur connecté

--- src/business/auth-data.service.ts
+++ src/business/auth-data.service.ts
@@ -6,77 +6,85 @@ import {Token} from '../../e-commerce-ui-common/models/Token';
 import {Router} from '@angular/router';
 import {MessageAlerte} from '../../e-commerce-ui-common/models/MessageAlerte';
 
-// noinspection JSAnnotator
-@Injectable()
-
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthDataService {
-  public utilisateur: Utilisateur = new Utilisateur(null, null, null, null, null);
-  public token: Token = new Token(this.utilisateur);
-  public messageAlerte: MessageAlerte = new MessageAlerte();
+  public token = new Token();
+  public messageAlerte = new MessageAlerte();
 
   constructor(private http: HttpClient, private _router: Router) {
-    if (localStorage.getItem('AuthToken') !== null && localStorage.getItem('AuthToken') !== undefined) {
-      this.token.token = localStorage.getItem('AuthToken');
+    if (sessionStorage.getItem('token') !== null) {
+      this.token.connecte = true;
+      this.token.token = sessionStorage.token;
+      this.token.utilisateur = JSON.parse(sessionStorage.user);
     }
   }
 
-  public async signIn() {
-    const response = await this.signInRequete();
-    if (response.valueOf()[0] !== undefined) {
-      this.messageAlerte.message = response.valueOf()[0]['message'];
-    } else if (response['signinUtilisateur'] !== undefined) {
+  public async signIn(email: string, mdp: string) {
+    const response = await this.signInRequete(email, mdp);
+    if (response['signinUtilisateur'] !== undefined) {
+      this.setToken(response['signinUtilisateur'].token.token, response['signinUtilisateur'].token.utilisateur);
       this.messageAlerte.message = '';
-      this.token.token = response['signinUtilisateur'].token.token;
-      localStorage.setItem('AuthToken', this.token.token);
-      localStorage.setItem('InfoUser', JSON.stringify(response['signinUtilisateur'].token.utilisateur));
       this._router.navigate(['/admin']);
-    }
-  }
-
-  public signInRequete() {
-    // On récupère l'objet Observable retourné par la requête post
-    if (this.utilisateur === undefined) {
-      // TODO erreur
-    }
-    const postResult = this.http.post(environment.api_login_url, {
-      query: 'mutation { signinUtilisateur(auth: {email:"' + this.utilisateur.email + '",' +
-        'password:"' + this.utilisateur.mdp + '"}){' +
-        'token{utilisateur{id email nom prenom}token}}}'
-    });
-    // On créer une promesse
-    const promise = new Promise<any>((resolve) => {
-      postResult
-      // On transforme en promesse
-        .toPromise()
-        .then(
-          response => {
-            // On résout notre promesse
-            resolve(response);
-          }
-        );
-    });
-    return promise;
-  }
-
-  public isLogged() {
-    let token;
-    if (this.token === undefined || this.token.token) {
-      token = localStorage.getItem('AuthToken');
-      if (token === null) {
-        token = 0;
-      }
     } else {
-      token = this.token.token;
+      console.log(response);
+      this.messageAlerte.message = response[0]['message'];
     }
+  }
+
+  public isLogged(): Promise<Boolean> {
+    if (!this.token.connecte) {
+      return new Promise<Boolean>(resolve => resolve(false));
+    }
+    return this.isLoggedRequest();
+  }
+
+  public logout() {
+    this.unsetToken();
+    this._router.navigate(['/admin/login']);
+  }
+
+  public isSet(): boolean {
+    return this.token.connecte;
+  }
+
+  public getToken(): string {
+    return this.token.token;
+  }
+
+  public getCurrentUser(): Utilisateur {
+    return this.token.utilisateur;
+  }
+
+  private setToken(token: string, utilisateur: Utilisateur): void {
+    this.token.token = token;
+    this.token.utilisateur = utilisateur;
+    this.token.connecte = true;
+    sessionStorage.setItem('token', token);
+    sessionStorage.setItem('user', '' + JSON.stringify(utilisateur));
+  }
+
+  private unsetToken(): void {
+    this.token.connecte = false;
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('user');
+  }
+
+  private signInRequete(email: string, mdp: string) {
+    return this.http.post(environment.api_login_url, {
+      query: 'mutation { signinUtilisateur(auth: {email:"' + email + '",' +
+        'password:"' + mdp + '"}){' +
+        'token{utilisateur{id email nom prenom}token}}}'
+    }).toPromise();
+  }
+
+  private isLoggedRequest(): Promise<Boolean> {
     // On récupère l'objet Observable retourné par la requête post
-    if (this.utilisateur === undefined) {
-      // TODO erreur
-    }
-    const postResult = this.http.post(environment.api_login_url, {query: 'mutation {isLogged(token:"' + token + '")}'});
+    const postResult = this.http.post(environment.api_login_url, {query: 'mutation {isLogged(token:"' + this.token.token + '")}'});
     // On créer une promesse
-    const promise = new Promise<any>((resolve) => {
+    return new Promise<any>((resolve) => {
       postResult
-      // On transforme en promesse
         .toPromise()
         .then(
           response => {
@@ -86,20 +94,8 @@ export class AuthDataService {
             } else {
               resolve(false);
             }
-            // On résout notre promesse
           }
         );
     });
-    return promise;
-  }
-
-  public logout() {
-    this.utilisateur.email = null;
-    this.utilisateur.mdp = null;
-    this.token.token = undefined;
-    this.token.utilisateur = new Utilisateur(null, null, null, null, null);
-    localStorage.removeItem('AuthToken');
-    localStorage.removeItem('InfoUser');
-    this._router.navigate(['/admin/login']);
   }
 }
