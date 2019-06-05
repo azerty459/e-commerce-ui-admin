c2b1d05521d1d773141ed429579a736f665a9d98
ludovic lahousse <llahousse@nextoo.fr>
2019-06-05 17:32:20 +0200

    commit du soir

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,7 @@ export const environment = {
   api_login_url: 'http://localhost:8888/graphql/login',
   api_rest_upload_url: 'http://localhost:8888/upload',
   api_rest_download_url: 'http://localhost:8888/fichier/',
-  api_url2: 'http://localhost:8888/utilisateur'
+  api_url2: 'http://localhost:8888/utilisateur',
+  api_url_product: 'http://localhost:8888/produit',
+  api_url_pagination: 'http://localhost:8888/pagination'
 };
